5c4d52b6bed4b70554c98487703fc6b5575e9723
Luarion <raulgamallo@gmail.com>
2026-07-01 13:30:29 +0200

    chore: add devcontainer build command to lint-staged configuration

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,4 +2,5 @@ module.exports = {
 	'*.{js,ts,jsx,tsx,json,jsonc,css,scss,html,md}': [
 		'biome check --write --no-errors-on-unmatched',
 	],
+	'{.devcontainer/devcontainer.json}': [() => 'devcontainer build'],
 };
