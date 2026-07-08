5f7a3379ce572d940e166aac4c39604e403ff9a6
Luarion <raulgamallo@gmail.com>
2026-07-08 12:23:30 +0000

    chore: configured a yaml linter on husky hooks

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,6 @@ module.exports = {
 	'*.{js,ts,jsx,tsx,json,jsonc,css,scss,html,md}': [
 		'biome check --write --no-errors-on-unmatched',
 	],
+	'*.{yaml,yml}': ['yamllint'],
 	'{.devcontainer/devcontainer.json}': [() => 'devcontainer build'],
 };
